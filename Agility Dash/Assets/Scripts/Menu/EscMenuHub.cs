b5a291ca538ab5a0e0f72a99ed46b8e2fe4b16cf
Valentin Gobl <84790546+Vali-G@users.noreply.github.com>
2023-06-13 23:36:19 +0200

    ESC-Menu Hub
    Esc Menu in Hub done

---
+++ Agility Dash/Assets/Scripts/Menu/EscMenuHub.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
+
+
+public class EscMenuHub : MonoBehaviour
+{
+
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+    public GameObject HUD;
+    public KeyCode pauseKey = KeyCode.Escape;
+    public KeyCode altPauseKey;
+
+    public GameObject pauseFirstButton, optionsFirstButton, optionsClosedButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(pauseKey) || Input.GetKeyDown(altPauseKey)))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            } else 
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        HUD.SetActive(true);
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        HUD.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Hub");
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void openSettings()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void closeSettings()
+    {
+        pauseMenuUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
+    }
+
+    
+}
